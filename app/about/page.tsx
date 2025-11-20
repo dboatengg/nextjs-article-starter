bca53c379b5fa5843bb274680f34ad8adae733c1
Dickson Boateng <dicksonboateng67@gmail.com>
2025-11-20 12:26:03 +0000

    Do some finishing touches

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,41 +1,73 @@
+// // import Link from "next/link";
+
+// // export default function About() {
+// //   return (
+// //     <main>
+// //       <h1>About This App</h1>
+// //       <p>This page lives at /about.</p>
+
+// //       <p>
+// //         <Link href="/">Back to Home</Link>
+// //       </p>
+// //     </main>
+// //   );
+// // }
+
+
+
+/*====================UPDATE 1=============================*/
 // import Link from "next/link";
+// import Image from "next/image";
 
 // export default function About() {
 //   return (
 //     <main>
-//       <h1>About This App</h1>
+//       <h1 className="text-2xl font-semibold mb-4">About This App</h1>
+
+//       <Image
+//         src="/test.png"
+//         alt="Laptop illustration"
+//         width={500}
+//         height={350}
+//         className="rounded-lg shadow-md mb-4"
+//       />
+
 //       <p>This page lives at /about.</p>
 
-//       <p>
-//         <Link href="/">Back to Home</Link>
+//       <p className="mt-4">
+//         <Link href="/" className="text-blue-600 hover:text-blue-800">
+//           Back to Home
+//         </Link>
 //       </p>
 //     </main>
 //   );
 // }
 
+
+/*====================UPDATE 2=============================*/
 import Link from "next/link";
 import Image from "next/image";
 
 export default function About() {
   return (
-    <main>
-      <h1 className="text-2xl font-semibold mb-4">About This App</h1>
+    <main className="flex flex-col items-start justify-center min-h-[80vh] space-y-6">
+      <div className="flex flex-col items-center md:flex-row gap-8 items-start md:items-center">
+        <Image
+          src="/test-image.webp"
+          alt="Test Image illustration"
+          width={400}
+          height={400}
+          className="rounded-lg shadow-md mb-4"
+        />
 
-      <Image
-        src="/test.png"
-        alt="Laptop illustration"
-        width={500}
-        height={350}
-        className="rounded-lg shadow-md mb-4"
-      />
+        <div>
+          <h1 className="text-2xl font-semibold mb-4">About This App</h1>
+          <p>
+            This Next.js project teaches you how to work with the Nextjs' core concepts to build a fully functional Movie Browsing Application.
+          </p>
 
-      <p>This page lives at /about.</p>
-
-      <p className="mt-4">
-        <Link href="/" className="text-blue-600 hover:text-blue-800">
-          Back to Home
-        </Link>
-      </p>
+        </div>
+      </div>
     </main>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,39 @@
+// import Link from "next/link";
+
+// export default function Home() {
+//   return (
+//     <main>
+//       <h1>Hello from Next.js</h1>
+//       <p>This is your first custom page.</p>
+
+//       <p>
+//         <Link href="/about">Go to About Page</Link>
+//       </p>
+//     </main>
+//   );
+// }
+
+/*====================UPDATE 1=============================*/
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello from Next.js</h1>
-      <p>This is your first custom page.</p>
+    <main className="flex flex-col items-start justify-center min-h-[80vh] space-y-6">
+      <h1 className="text-2xl font-semibold mb-4">Welcome to the Movie Browsing App</h1>
 
-      <p>
-        <Link href="/about">Go to About Page</Link>
+      <p className="mb-4">
+        Explore popular movies, search your favorites, and view detailed
+        information.
+      </p>
+
+      <p className="space-x-4">
+        <Link href="/movies" className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
+          Browse Movies
+        </Link>
+
+        <Link href="/about" className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
+          About This Project
+        </Link>
       </p>
     </main>
   );
